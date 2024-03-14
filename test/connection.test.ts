48df3fe726673ea8b45824709700e6a6a9e5fe66
Andrei Ruan Steffens <andreiruansteffens@gmail.com>
2024-03-14 13:45:33 -0300

    Finalizado teste de conexão

--- test/connection.test.ts
+++ test/connection.test.ts
@@ -5,5 +5,5 @@ test("Deve ciar uma connexão com o banco de dados PostgreSQL", async function (
     const connection = new PostgreSQLConnection(configDatabase);
     const [output] = await connection.execute('SHOW SERVER_VERSION;');
     await connection.close();
-    expect(output['server_version']).toBe('14.10 (Ubuntu 14.10-0ubuntu0.22.04.1)');
+    expect(output['server_version']).toBeTruthy();
 });
